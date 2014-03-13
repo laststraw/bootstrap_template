53878e78f04fb0fc362edbaebe0a02a8aa5d4554
Sean Lovell <csean.lovell@gmail.com>
2014-03-13 12:18:23 +0700

    Added sidebar, still lacks change color on active
    Static sidebar, scrollspy works, but list item color doesn't change when
    "active" class is applied

--- js/main.js
+++ js/main.js
@@ -1,1 +1,146 @@
 
+// Cache selectors
+var lastId,
+    topMenu = $("#scrolltarget"),
+    topMenuHeight = -15,
+// All list items
+    menuItems = topMenu.find("a"),
+// Anchors corresponding to menu items
+    scrollItems = menuItems.map(function () {
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function (e) {
+    var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 300);
+    e.preventDefault();
+});
+
+var margin = { top: 20, right: 20, bottom: 30, left: 60 },
+			width = 960 - margin.left - margin.right,
+			height = 500 - margin.top - margin.bottom;
+
+var x0 = d3.scale.ordinal()
+			.rangeRoundBands([0, width], .1);
+
+var x1 = d3.scale.ordinal();
+
+var y = d3.scale.linear()
+			.range([height, 0]);
+
+var color = d3.scale.ordinal()
+			.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+var xAxis = d3.svg.axis()
+			.scale(x0)
+			.orient("bottom");
+
+var yAxis = d3.svg.axis()
+			.scale(y)
+			.orient("left")
+			.tickFormat(d3.format(".2s"));
+
+var svg = d3.select("#divd3_1").append("svg")
+			.attr("width", width + margin.left + margin.right)
+			.attr("height", height + margin.top + margin.bottom)
+		    .append("g")
+			.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("bar.csv", function (error, data) {
+    var ageNames = d3.keys(data[0]).filter(function (key) { return key !== "State"; });
+
+    data.forEach(function (d) {
+        d.ages = ageNames.map(function (name) { return { name: name, value: +d[name] }; });
+    });
+
+    x0.domain(data.map(function (d) { return d.State; }));
+    x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
+    y.domain([0, d3.max(data, function (d) { return d3.max(d.ages, function (d) { return d.value; }); })]);
+
+    svg.append("g")
+			  .attr("class", "x axis")
+			  .attr("transform", "translate(0," + height + ")")
+			  .call(xAxis);
+
+    svg.append("g")
+			  .attr("class", "y axis")
+			  .call(yAxis)
+			  .append("text")
+			  .attr("transform", "rotate(-90)")
+			  .attr("y", 6)
+			  .attr("dy", ".71em")
+			  .style("text-anchor", "end")
+			  .text("Population");
+
+    var state = svg.selectAll(".state")
+			  .data(data)
+			  .enter().append("g")
+			  .attr("class", "g")
+			  .attr("transform", function (d) { return "translate(" + x0(d.State) + ",0)"; });
+
+    state.selectAll("rect")
+			  .data(function (d) { return d.ages; })
+			  .enter().append("rect")
+			  .attr("width", x1.rangeBand())
+			  .attr("x", function (d) { return x1(d.name); })
+			  .attr("y", function (d) { return y(d.value); })
+			  .attr("height", function (d) { return height - y(d.value); })
+			  .style("fill", function (d) { return color(d.name); });
+
+    var legend = svg.selectAll(".legend")
+			  .data(ageNames.slice().reverse())
+			  .enter().append("g")
+			  .attr("class", "legend")
+			  .attr("transform", function (d, i) { return "translate(0," + i * 20 + ")"; });
+
+    legend.append("rect")
+			  .attr("x", width - 18)
+			  .attr("width", 18)
+			  .attr("height", 18)
+			  .style("fill", color);
+
+    legend.append("text")
+			  .attr("x", width - 24)
+			  .attr("y", 9)
+			  .attr("dy", ".35em")
+			  .style("text-anchor", "end")
+			  .text(function (d) { return d; });
+
+});
+
+// Bind to scroll
+$(window).scroll(function () {
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function () {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#" + id + "]").parent().addClass("active");
+    }
+});
+
+
+var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+(function (d, t) {
+    var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+    g.src = '//www.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g, s)
+} (document, 'script'));
