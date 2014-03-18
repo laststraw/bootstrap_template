7f89c3bbe8ff60d73114a230eb003a176151eea4
Sean Lovell <csean.lovell@gmail.com>
2014-03-18 15:53:57 +0700

    Fixed javascript loading

--- js/main.js
+++ js/main.js
@@ -151,11 +151,12 @@ $(document).ready(function () {
         }
     });
 
+    var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
+    (function (d, t) {
+        var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        g.src = '//www.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g, s)
+    } (document, 'script'));
+
 });
 
-var _gaq = [['_setAccount', 'UA-XXXXX-X'], ['_trackPageview']];
-(function (d, t) {
-    var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
-    g.src = '//www.google-analytics.com/ga.js';
-    s.parentNode.insertBefore(g, s)
-} (document, 'script'));
